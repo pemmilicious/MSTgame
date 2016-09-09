877ce3c029e73a648e092b5bca794bb6d36159c1
pemmilicious <bradley.pemmelaar@my.jcu.edu.au>
2016-09-09 15:16:44 +1000

    start game

--- source/MST.java
+++ source/MST.java
@@ -1,7 +1,34 @@
+import javax.swing.*;
 public class MST {
     public static void main(String[] args) {
         welcomeMessage();
+        displayMenu();
+    }
 
+    private static int getUserMenuChoice() {
+        return 0;
+    }
+
+    private static void displayMenu() {
+        String usersChoiceString;
+        int usersChoice = 0;
+        usersChoiceString = JOptionPane.showInputDialog(null,
+                "Enter 1 or 2\n1 - Start Game\n2 - Exit");
+        usersChoice = Integer.parseInt(usersChoiceString);
+                if (usersChoice == 1){
+                    gameStart();}
+                else{
+                    exitGame();
+                    }
+
+
+    }
+
+    private static void exitGame() {
+        System.out.println("catchya later");
+    }
+
+    private static void gameStart() {
     }
 
     private static void welcomeMessage() {
