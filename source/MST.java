f59293975a53a9900ed0259b5b65062de0c495a4
pemmilicious <bradley.pemmelaar@my.jcu.edu.au>
2016-09-09 14:17:13 +1000

    welcome

---
+++ source/MST.java
@@ -0,0 +1,10 @@
+public class MST {
+    public static void main(String[] args) {
+        welcomeMessage();
+
+    }
+
+    private static void welcomeMessage() {
+        System.out.println("hello");
+    }
+}
